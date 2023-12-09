484f8936eacacd7845583b024698a96ad02280fe
Marcelo-Negrier <marcelo.negrier@acroventus.com>
2023-12-08 23:11:36 -0300

    cambios en la web

--- assets/Toast.37c00292.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as o,t as c,O as n}from"./index.7780f7a9.js";const _={__name:"Toast",props:["message","type"],setup(t){const e=t;return(r,p)=>(a(),o("div",{id:"toast",class:"shadow-5 flex align-items-center justify-content-center",style:n("background: "+e.type+";")},c(e.message),5))}},i=s(_,[["__scopeId","data-v-c903a8c2"]]);export{i as T};
